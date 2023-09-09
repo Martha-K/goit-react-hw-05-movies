430d2fcac528d347d2271d725bb3f3d8070b3b66
Martha-K <kozenyukmarta@gmail.com>
2023-09-09 13:52:30 +0300

    fixed linter error

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -12,6 +12,7 @@ const Movies = () => {
 
   useEffect(() => {
     movieId && onSearch(movieId);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const onSearch = search => {
